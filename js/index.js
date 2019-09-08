3e0666d4a14437b4266a8ade215a4119ff750897
mikejeter <mjeter45@yahoo.com>
2019-09-08 02:54:32 -0400

    modified index.js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,26 @@
 // Your code goes here
+const firstNavItem = document.querySelector('.nav-link');
+firstNavItem.addEventListener('mouseover', (event) => {
+    event.target.style.color = "red";
+});
+
+// const allNavItems = document.querySelectorAll('.nav-link');
+// allNavItems.forEach((items) => {
+//     items.addEventListener('mouseorver', textColor);
+// });
+
+// function textColor(event) {
+//     event.target.style.color = "red";
+// }
+
+document.addEventListener('keydown', logKey);
+
+function logKey(e) {
+    log.textContent += ` ${e.code}`;
+}
+
+const button = document.querySelector('.btn');
+
+button.addEventListener('click', event => {
+    button.innerHTML = `Click count: ${event.detail}`;
+});
